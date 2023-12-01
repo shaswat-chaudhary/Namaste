f5023cd090c5c0a825e11ca0d1b2c24ba175ead4
iamshaswat <shaswatkumar281@bbdu.ac.in>
2023-12-02 01:59:44 +0530

    change

--- config/database.js
+++ config/database.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbConnect = async () => {
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
 
         .then(() => console.log("Database Connection is Successfully"))
         .catch((error) => {
